91b4da31005def710ef28deee06f752c1fec19c9
SameerSS19 <sk3090019@gmail.com>
2021-08-09 20:01:39 +0530

    [Sameer]Add:Solution Day4

---
+++ src/main/java/com/bridgelabz/Day1/ReverseString.java
@@ -0,0 +1,26 @@
+package com.bridgelabz.Day1;
+
+import java.util.Scanner;
+public class ReverseString {
+    public static void main(String[] args)
+    {
+        String str;
+        System.out.println("Enter a string: ");
+        Scanner scanner = new Scanner(System.in);
+        str = scanner.nextLine();
+        String str1 = " ";
+        String a[] = str.split(" ");
+        for (int i=0;i< a.length;i++)
+        {
+            System.out.print(a[i]+" ");
+        }
+        System.out.println(" ");
+        for (int i=a.length - 1; i >= 0; i--)
+        {
+            str1= str1+a[i]+" ";
+        }
+        System.out.println("Reverse Words is :" +str1);
+
+    }
+}
+
