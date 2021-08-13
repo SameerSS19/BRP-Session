b83a308c4a0a4c72d1cb76bbb9395b4f6201fc9f
SameerSS19 <sk3090019@gmail.com>
2021-08-13 21:01:25 +0530

    [Sameer]Add:Solution of Day8

--- src/main/java/com/practiceproblem/SortDecending.java
+++ src/main/java/com/practiceproblem/SortDecending.java
@@ -12,7 +12,6 @@ public class SortDecending {
         int size=sc.nextInt();
         int n1 = size%2==0?(size/2):(size/2)+1;
         int n2 = size-n1;
-        int n3;
         ArrayList<Integer> first=new ArrayList<>();
         ArrayList<Integer> second =new ArrayList<>();
         System.out.println("Enter the Element of Array:");
@@ -21,22 +20,18 @@ public class SortDecending {
 
         for (int i=0;i<n2;i++)
         second.add(sc.nextInt());
-        System.out.println("Choose the Element of Array:");
-        n3 = sc.nextInt();
-        System.out.println(n3);
+//        System.out.println("Choose the Element of Array:");
+//        temp= sc.nextInt();
+//        System.out.println(temp);
 
 
         Collections.sort(first,Collections.reverseOrder());
         Collections.sort(second,Collections.reverseOrder());
 
         for (int i=0;i<n1;i++)
-            System.out.println(first.get(i));
+            System.out.println(+first.get(i));
 
         for (int i=0;i<n2;i++)
             System.out.println(second.get(i));
-
-
-
-    }
 }
-
+}
