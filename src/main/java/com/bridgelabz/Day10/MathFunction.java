c2531b634bd6102d12419c639e5404a67e606f68
Shakiv Ali <56513506+shak160@users.noreply.github.com>
2021-09-04 00:16:30 +0530

    [Sameer]Add:Solution Day 10

--- src/main/java/com/bridgelabz/Day10/MathFunction.java
+++ src/main/java/com/bridgelabz/Day10/MathFunction.java
@@ -9,4 +9,26 @@ public class MathFunction {
     public static void presentValue(double presentAmount) {
         System.out.println("Your Present Value is : "+presentAmount);
     }
+
+    // Method for getting the maximum value
+    public static int getMax(int[] inputArray){
+        int maxValue = inputArray[0];
+        for(int i=1;i < inputArray.length;i++){
+            if(inputArray[i] > maxValue){
+                maxValue = inputArray[i];
+            }
+        }
+        return maxValue;
+    }
+
+    // Method for getting the minimum value
+    public static int getMin(int[] inputArray){
+        int minValue = inputArray[0];
+        for(int i=1;i<inputArray.length;i++){
+            if(inputArray[i] < minValue){
+                minValue = inputArray[i];
+            }
+        }
+        return minValue;
+    }
 }
